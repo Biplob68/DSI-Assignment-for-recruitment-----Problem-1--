078c3a7ef556d93609e300500021d30d36cda7bd
Md. Biplob Mina <66861180+Biplob68@users.noreply.github.com>
2021-03-27 17:24:49 +0600

    admin

---
+++ availableBalance.java
@@ -0,0 +1,6 @@
+package business_site;
+
+public class availableBalance {
+	
+	static int balance = 0; 
+}
---
+++ main.java
@@ -0,0 +1,13 @@
+package business_site;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		
+		SmallShop market = new SmallShop(); 
+		market.Shop(); 
+
+	}
+
+}
